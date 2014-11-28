3f71b65ab763fad643388769de16fe5204ea9d09
Manu Gupta <manugupt1@gmail.com>
2014-11-28 01:27:43 -0500

    Adding R file

--- r_file.R
+++ r_file.R
@@ -1,8 +1,7 @@
 library('Quandl')
 
 auth_code <- read.table(file="GitHub/life_expectancy/auth_code.R")
-View(auth_code)
-Quandl.auth("ffT8ALix-1Z9XisyY5iB")
+Quandl.auth(auth_code$V1)
 
 health_expenditure <- Quandl("WORLDBANK/SLE_SH_XPD_PCAP_PP_KD", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
 names(health_expenditure)[2]<-"Health_Expenditure"
