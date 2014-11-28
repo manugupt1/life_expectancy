2353e7473bd3abf08b762f1f893988e97340cd3a
Manu Gupta <manugupt1@gmail.com>
2014-11-28 01:24:50 -0500

    Remove auth code

--- r_file.R
+++ r_file.R
@@ -1,4 +1,7 @@
 library('Quandl')
+
+auth_code <- read.table(file="GitHub/life_expectancy/auth_code.R")
+View(auth_code)
 Quandl.auth("ffT8ALix-1Z9XisyY5iB")
 
 health_expenditure <- Quandl("WORLDBANK/SLE_SH_XPD_PCAP_PP_KD", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
@@ -16,5 +19,8 @@ plot(main = "Sierra Leone: Percentage of People Undernourished",x= percent_under
 
 accesss_to_improved_water_sources <- Quandl("WORLDBANK/SLE_SH_H2O_SAFE_ZS", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
 names(accesss_to_improved_water_sources)[2]<- "accesss_to_improved_water_sources_percent"
-plot(main = "mproved water source refers to the percentage of the population with reasonable access to an adequate amount of water from an improved source",x= accesss_to_improved_water_sources$Date,y=accesss_to_improved_water_sources$accesss_to_improved_water_sources_percent)
+plot(main = "Sierra Leone :mproved water source refers to the percentage of the population with reasonable access to an adequate amount of water from an improved source",x= accesss_to_improved_water_sources$Date,y=accesss_to_improved_water_sources$accesss_to_improved_water_sources_percent)
 
+access_to_improved_sanitation <- Quandl("WORLDBANK/SLE_SH_STA_ACSN", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
+names(access_to_improved_sanitation)[2]<-"access_to_improved_sanitation_percent"
+plot(main="Sierra Leone: Improved sanitation facilities (% of population",x=access_to_improved_sanitation$Date,y=access_to_improved_sanitation$access_to_improved_sanitation_percent)
