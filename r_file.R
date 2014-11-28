56086386ddf61594872037146076fd4f9631e0a3
Manu Gupta <manugupt1@gmail.com>
2014-11-28 01:17:47 -0500

    -- Added more code, more plots

--- r_file.R
+++ r_file.R
@@ -1,8 +1,20 @@
 library('Quandl')
+Quandl.auth("ffT8ALix-1Z9XisyY5iB")
+
+health_expenditure <- Quandl("WORLDBANK/SLE_SH_XPD_PCAP_PP_KD", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
+names(health_expenditure)[2]<-"Health_Expenditure"
+plot(main = "Sierra Leone: Health expenditure per capita, PPP (constant 2005 international $)",x= health_expenditure$Date,y=health_expenditure$Health_Expenditure)
+
+community_health_workers_per_1000 <- Quandl("WORLDBANK/SLE_SH_MED_CMHW_P3", trim_start="1975-12-31", trim_end="2010-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
+names(community_health_workers_per_1000)[2]<-"Community_Health_Workers_per_1000"
+plot(main = "Sierra Leone: Community health workers (per 1,000 people)",x= community_health_workers_per_1000$Date,y=community_health_workers_per_1000$Community_Health_Workers_per_1000)
 
 
-health_expenditure_in_years <- Quandl("WORLDBANK/SLE_SH_XPD_PCAP_PP_KD", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
+percent_undernourished <- Quandl("WORLDBANK/SLE_SN_ITK_DEFC_ZS", trim_start="1975-12-31", trim_end="2012-12-31")
+names(percent_undernourished)[2]<-"Percent_Undernourished"
+plot(main = "Sierra Leone: Percentage of People Undernourished",x= percent_undernourished$Date,y=percent_undernourished$Percent_Undernourished)
 
-names(health_expenditure_in_years)[2]<-"Health_Expenditure"
-View(health_expenditure_in_years)
-plot(main = "Sierra Leone: Health expenditure per capita, PPP (constant 2005 international $)",x= health_expenditure_in_years$Date,y=health_expenditure_in_years$Health_Expenditure)
+accesss_to_improved_water_sources <- Quandl("WORLDBANK/SLE_SH_H2O_SAFE_ZS", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
+names(accesss_to_improved_water_sources)[2]<- "accesss_to_improved_water_sources_percent"
+plot(main = "mproved water source refers to the percentage of the population with reasonable access to an adequate amount of water from an improved source",x= accesss_to_improved_water_sources$Date,y=accesss_to_improved_water_sources$accesss_to_improved_water_sources_percent)
+
