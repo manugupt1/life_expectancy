617128d642c59b8c043d610560c89c1ffab9a791
Manu Gupta <manugupt1@gmail.com>
2014-12-06 17:45:06 -0500

    A regression model for LifeExpectancy in African Countries

--- r_file.R
+++ r_file.R
@@ -1,25 +1,20 @@
-library('Quandl')
+csv = read.csv(file="dataset_2.csv")
 
-auth_code <- read.table(file="GitHub/life_expectancy/auth_code.R")
-Quandl.auth(auth_code$V1)
+csv_sl = csv[csv$Country=="Sierra Leone",]
+csv_sl.lm = lm(Life.Expectancy~War+Health.Expenditure+I(Improved.Water.Sources*Improved.Sanitation)+Percentage.of.People.Undernourished, data=csv_sl)
+summary(csv_sl.lm)
+newdata = data.frame(War=0,Health.Expenditure=122.3210644,Percentage.of.People.Undernourished=39.9,Improved.Water.Sources=50.4,Improved.Sanitation=12.2)
+predict(csv_sl.lm,newdata)
 
-health_expenditure <- Quandl("WORLDBANK/SLE_SH_XPD_PCAP_PP_KD", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
-names(health_expenditure)[2]<-"Health_Expenditure"
-plot(main = "Sierra Leone: Health expenditure per capita, PPP (constant 2005 international $)",x= health_expenditure$Date,y=health_expenditure$Health_Expenditure)
+csv_chad = csv[csv$Country=="Chad",]
+csv_chad.lm = lm(Life.Expectancy~War+Health.Expenditure+I(Improved.Water.Sources*Improved.Sanitation)+Percentage.of.People.Undernourished, data=csv_chad)
+summary(csv_chad.lm)
+newdata = data.frame(War=0,Health.Expenditure=58.53,Percentage.of.People.Undernourished=37.9,Improved.Water.Sources=46.7,Improved.Sanitation=10.4)
+predict(csv_chad.lm,newdata)
 
-community_health_workers_per_1000 <- Quandl("WORLDBANK/SLE_SH_MED_CMHW_P3", trim_start="1975-12-31", trim_end="2010-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
-names(community_health_workers_per_1000)[2]<-"Community_Health_Workers_per_1000"
-plot(main = "Sierra Leone: Community health workers (per 1,000 people)",x= community_health_workers_per_1000$Date,y=community_health_workers_per_1000$Community_Health_Workers_per_1000)
+csv_senegal = csv[csv$Country=="Senegal",]
+csv_senegal.lm = lm(Life.Expectancy~War+Health.Expenditure+I(Improved.Water.Sources*Improved.Sanitation)+Percentage.of.People.Undernourished, data=csv_senegal)
+summary(csv_senegal.lm)
 
-
-percent_undernourished <- Quandl("WORLDBANK/SLE_SN_ITK_DEFC_ZS", trim_start="1975-12-31", trim_end="2012-12-31")
-names(percent_undernourished)[2]<-"Percent_Undernourished"
-plot(main = "Sierra Leone: Percentage of People Undernourished",x= percent_undernourished$Date,y=percent_undernourished$Percent_Undernourished)
-
-accesss_to_improved_water_sources <- Quandl("WORLDBANK/SLE_SH_H2O_SAFE_ZS", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
-names(accesss_to_improved_water_sources)[2]<- "accesss_to_improved_water_sources_percent"
-plot(main = "Sierra Leone :mproved water source refers to the percentage of the population with reasonable access to an adequate amount of water from an improved source",x= accesss_to_improved_water_sources$Date,y=accesss_to_improved_water_sources$accesss_to_improved_water_sources_percent)
-
-access_to_improved_sanitation <- Quandl("WORLDBANK/SLE_SH_STA_ACSN", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
-names(access_to_improved_sanitation)[2]<-"access_to_improved_sanitation_percent"
-plot(main="Sierra Leone: Improved sanitation facilities (% of population",x=access_to_improved_sanitation$Date,y=access_to_improved_sanitation$access_to_improved_sanitation_percent)
+csv.lm = lm(Life.Expectancy~Country+War+Health.Expenditure+I(Improved.Water.Sources*Improved.Sanitation)+Percentage.of.People.Undernourished, data=csv)
+summary(csv.lm)
