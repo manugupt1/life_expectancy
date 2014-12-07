bb609240822c1afe7e68f37addb0722c1446de41
dcdinicola <dinicol@umich.edu>
2014-12-07 10:34:29 -0500

    Added updates to the presentation - needs plots

--- r_file.R
+++ r_file.R
@@ -1,7 +1,7 @@
 library('Quandl')
 
 auth_code <- read.table(file="GitHub/life_expectancy/auth_code.R")
-Quandl.auth(auth_code$V1)
+Quandl.auth(2oDc_ujzjRX8_QV_nfFJ)
 
 health_expenditure <- Quandl("WORLDBANK/SLE_SH_XPD_PCAP_PP_KD", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
 names(health_expenditure)[2]<-"Health_Expenditure"
@@ -23,3 +23,12 @@ plot(main = "Sierra Leone :mproved water source refers to the percentage of the 
 access_to_improved_sanitation <- Quandl("WORLDBANK/SLE_SH_STA_ACSN", trim_start="1975-12-31", trim_end="2012-12-31", authcode="ffT8ALix-1Z9XisyY5iB")
 names(access_to_improved_sanitation)[2]<-"access_to_improved_sanitation_percent"
 plot(main="Sierra Leone: Improved sanitation facilities (% of population",x=access_to_improved_sanitation$Date,y=access_to_improved_sanitation$access_to_improved_sanitation_percent)
+
+
+percent_undernourished <- Quandl("WORLDBANK/SLE_SN_ITK_DEFC_ZS", trim_start="1975-12-31", trim_end="2012-12-31")
+names(percent_undernourished)[2]<-"Percent_Undernourished"
+plot(main = "Sierra Leone: Percentage of People Undernourished",x= percent_undernourished$Date,y=percent_undernourished$Percent_Undernourished)
+Enrollment <- Quandl("UN/UIS_LOWERSECONDARYEDUCATIONENROLMENTINALLPROGRAMMES__ALLGENDERS_SLE" trim_start="2001-12-31", trim_end="2012-12-31" authcode="2oDc_ujzjRX8_QV_nfFJ"
+names(number_of_students_enrolled)[2] <- "Students_Enrolled_Lower_Secondary"
+plot(main = "Sierra Leone:  Number of Students Enrolled, Lower Secondary", x=Enrollment$date, y=Enrollment$Enrollment)
+
